2a2ac5d0529174feeb7168a2aa80edcf7173ff83
istifano <elamiriamir7@gmail.com>
2024-12-27 11:11:02 +0100

    j ai apris comment travailler avec le principe de Encapculation ,What Are Getters and Setters

--- University & Subjects/Student.php
+++ University & Subjects/Student.php
@@ -13,6 +13,36 @@ class Student
     public string $name;
     public string $studentNumber;
 
+    function __construct($name,$studentNumber)
+    {
+      $this->name=$name;
+      $this->studentNumber=$studentNumber;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getStudentNumber()
+    {
+        return $this->studentNumber;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function setStudentNumber($studentNumber)
+    {
+        $this->studentNumber = $studentNumber;
+    }
+
     // TODO Generate getters and setters for both arguments
     // TODO Generate constructor with both arguments.
 }
+
+// $var = new Student("Aamir",2003);
+// echo$var->getName();
+
+// $var->setName("wassime");
+// var_dump($var);
+
+
